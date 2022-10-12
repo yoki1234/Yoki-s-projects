d4817cecd60f3252d6f43aeea9147df706270dbe
akshay_ashok <akshayashokdj@gmail.com>
2022-10-12 16:30:59 +0530

    feat:class-components

---
+++ learningReact/class-components/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ClassComponent from './component/ClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent/>
+    </div>
+  );
+}
+
+export default App;
