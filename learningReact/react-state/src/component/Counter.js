3af47e55935933dfb44913e2a082431a14215095
akshay_ashok <akshayashokdj@gmail.com>
2022-10-13 09:53:28 +0530

    react-setState

---
+++ learningReact/react-state/src/component/Counter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+ 
+   
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count:0
+      }
+    }
+    increment(){
+        this.setState({
+            count:this.state.count+1
+        },()=>{console.log('callBack',this.state.count)})
+        // this.state.count = this.state.count+1;
+        // console.log(this.state.count);
+    }
+    Decrement(){
+        this.setState({
+            count:this.state.count-1
+        },()=>{console.log('callBack',this.state.count)})
+    }
+    }
+  render() {
+    return (
+        <div>
+             <div>Count-{this.state.count}</div>
+             <button onClick={()=>{this.increment()}}>Increment</button>
+             <button onClick={()=>{this.Decrement()}}>Decrement</button>
+        </div>
+     
+    )
+  }
+}
+
+export default Counter
